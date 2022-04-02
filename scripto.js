69d5abd8e9b462f67a62d024dc608e104d2f16ae
sumit <sumitghuraiya@gmail.com>
2022-04-03 00:54:22 +0530

    added_more

--- scripto.js
+++ scripto.js
@@ -5,3 +5,10 @@ var typed=new Typed(".typing",
     backSpeed:60,
     loop:true
 });
+var typed=new Typed(".typ",
+{
+    strings: ["Web developer","Competitive Programmer","Machine learning enthusiast"]
+    ,typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
